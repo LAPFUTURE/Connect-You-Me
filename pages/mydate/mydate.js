e30571ff863df38cdb91e795f66ee4d0f2623fbc
LAP <2300820209@qq.com>
2018-10-08 22:42:20 +0800

    删除 mydate

--- pages/mydate/mydate.js
+++ pages/mydate/mydate.js
@@ -2,12 +2,14 @@ const app = getApp();
 Page({
     data:{
        myDate:'',
-       datePage:2
+       datePage:2,
+       url:app.globalData.url,
+       //不在这里使用openId:app.globalData.openId是因为页面初始化时app.globalData.openId可能还没有回来，所以可以将
+       //openId:app.globalData.openId在下面的点击触发时再进行赋值，这样不存在异步的问题
     },
     onShow:function(){
-        console.time("b");
         this.setData({
-            myDate:wx.getStorageSync("myDate")
+            myDate:wx.getStorageSync("myDate"),
         });
     },
     onReachBottom:function(){
@@ -16,17 +18,18 @@ Page({
         });
         let that = this;
         wx.request({
-            url: app.globalData.url + "userdate",
+            url:this.data.url + "mydate",
             method:"post",
             header:{"content-type":"application/x-www-form-urlencoded"},
             data:{
-              openId:app.globalData.openId,
+              openId:this.data.openId,
               datePage:this.data.datePage
             },
             success:function(res){
                 if(res.data.status == 1){
                   that.setData({
-                    myDate:that.data.myDate.concat(res.data.dates)  
+                    myDate:that.data.myDate.concat(res.data.dates),
+                    datePage:(this.data.datePage + 1)
                   });
                   wx.hideLoading();
                 }else{
@@ -37,9 +40,50 @@ Page({
                 }
             }
         })
-    },
+    },  
     
     deleteMyDate:function(e){
-        console.log(e.detail);
+        // console.log(app.globalData.openId);
+        // console.log(this.data.openId);
+        let that = this;
+        let dateId = e.currentTarget.dataset.dateid;
+        wx.request({
+          url: this.data.url + "deleteMyDate",
+          method:"post",
+          header:{'content-type':'application/x-www-form-urlencoded'},
+          data:{
+            openId:app.globalData.openId,
+            dateId:dateId
+          },
+          success:function(res){ 
+            // console.log("myDate:",that.data.myDate);
+            let status = res.data.status;
+            if(status == 1){
+              let length = that.data.myDate.length;
+              let arr = that.data.myDate;
+              for(let i = 0; i < length; i++){
+                console.log("arr[i]:",arr[i].dateId);
+                console.log("i:",i);
+                if(arr[i].dateId == dateId){
+                  console.log("找到了");
+                 arr.splice(i,1);
+                 break;
+                }
+              }
+              wx.showToast({
+                title:"删除成功"
+              });
+            }else if(status == 0 ){
+              wx.showToast({
+                title:"删除失败"
+              });
+            }else{
+              wx.showToast({
+                title:"参数错误"
+              });
+            }
+
+          }
+        });
     }
 })
